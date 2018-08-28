23a2670a7db1d15554d92b34f8c8185ee169d91f
JoSaGuDu <joseguti5@gmail.com>
2018-08-28 16:02:58 -0400

    module scaffolding added

---
+++ main.js
@@ -0,0 +1,35 @@
+//create my module
+const displayerModule = {
+  //module configuration
+  moduleConfig: {
+    $container: $('content'),
+  },
+
+  //init module
+  init: function (customModuleConfig) {
+    //build navigation system
+  },
+
+  //navigation system builder
+  enableNavSystem: function () {
+
+  },
+
+  //show selected section
+  showSection: function () {
+    //build inSectionNavSystem
+  },
+
+  //build in section navigation system
+  enableInSectionNavSystem: function () {
+    //show selected content
+  },
+
+  //show selected content
+  showContent: function () {
+
+  }
+
+};
+
+$(document).ready(displayerModule.init({}));
